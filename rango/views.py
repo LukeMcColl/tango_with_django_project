508dcf0d89825d8cbb7540d9f90cd2934382f1b3
Luke McColl <2578891m@student.gla.ac.uk>
2023-02-08 11:33:16 +0000

    End of Chapter 4

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    #construct a dictionary to pass the template engine as its context
+    #note the key boldmessage matches to {{ boldmessage }} in the template:
+    
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    #return a rendered response to send to the client
+    #We make use of the shortcut function to make our lives easier
+    #note that the first parameter is the template we want to use
+    
+    return render(request, 'rango/index.html', context=context_dict)
     
 # Create your views here.
