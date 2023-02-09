923dc07a537cb7c21ea340b2b7a0fe1b51f3ebf0
Luke McColl <2578891m@student.gla.ac.uk>
2023-02-09 13:04:56 +0000

    End of Chapter 7

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -21,8 +21,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     #this means any URL starting with rango/ is handled by rango.
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+    path('add_category/', views.add_category, name='add_category'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
